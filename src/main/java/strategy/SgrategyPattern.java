27d800294115c6021ec069f4e2cf7cdb5e6a92fe
watermelon <icewatermelon88@gmail.com>
2021-04-04 12:33:54 +0900

    strategyPattern

---
+++ src/main/java/strategy/SgrategyPattern.java
@@ -0,0 +1,18 @@
+package strategy;
+
+public class SgrategyPattern {
+
+    public static void main(String[] args) {
+        Player player1 = new Player("player1", new RockStrategy());
+        Player player2 = new Player("player2", new PaperStrategy());
+        Player player3 = new Player("player3", new RandomStrategy());
+        Player player4 = new Player("player4", () -> Hand.Scissors);
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(player1.nextHand().fight(player3.nextHand()));
+        }
+
+
+    }
+}
